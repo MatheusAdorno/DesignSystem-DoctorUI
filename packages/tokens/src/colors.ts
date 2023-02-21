fb8e12afdbb82955c9089121e868722fbe5a2e48
Matheus Adorno <matheusadorno11@gmail.com>
2023-02-21 18:38:48 -0300

    Rest of Package Folder (React and Tokens)

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  blue300: '#0169CB',
+  blue500: '#024E97',
+  blue700: '#003B73',
+  blue900: '#011A32',
+}
